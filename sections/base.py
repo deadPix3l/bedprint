43e0ce2579bb52935a2779aab402323918235e78
Skyler Curtis <skylerr.curtis@gmail.com>
2025-08-05 04:43:56 -0400

    structuring

---
+++ sections/base.py
@@ -0,0 +1,17 @@
+class Section:
+    """A Section on the tape"""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def bulleted_list(self, bullet: str = "-"):
+        pass
+
+    def printout(self, p):
+        pass
+
+
+class ImageSection(Section):
+    pass
+
+        
